030ed96b7c5e3040094aa7717acf98d048f98eb6
siponek <szinkowicz@gmail.com>
2024-05-12 14:07:58 +0200

    feat: Added main.py with Molloy-Reed Criterion, attack strategies simulation and plotting

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
+from numpy.random import Generator, SeedSequence, MT19937
 import numpy as np
 from copy import deepcopy
 from math import sqrt
 from pathlib import Path
 from multiprocessing import Pool
+import pandas as pd
+import seaborn as sns
+import csv
 
 
 def random_attack(graph: nx.Graph) -> list:
@@ -78,8 +82,8 @@ def setup_graph():
     print(f"Using dataset from {DATA_PATH.absolute()}")
     G = nx.read_edgelist(path=DATA_PATH, create_using=nx.Graph(), nodetype=int)
     G.remove_edges_from(nx.selfloop_edges(G))
-    G = nx.k_core(G, k=2)
     print("nodes:", len(G.nodes()), "and edges:", len(G.edges()))
+    G = nx.k_core(G, k=2)
     pos = nx.spring_layout(G, seed=SEED, k=15 / sqrt(len(G.nodes())))
     infected_time_init: int = 0
     nx.set_node_attributes(G, "S", "state")
@@ -106,28 +110,40 @@ def plot_and_save(graph_name: str, result_dict: dict, attack_modes: list[str]):
     plot_folder_out = Path("plots")
     plot_folder_out.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
 
+    # Preparing data for plotting
+    data1 = []
+    data2 = []
+    for mode in attack_modes:
+        x, y, z = result_dict[mode]
+        data1.extend([(xx, yy, mode) for xx, yy in zip(x, y)])
+        data2.extend([(xx, zz, mode) for xx, zz in zip(x, z)])
+    STR_CONST_1: str = "Fraction of Nodes Removed"
+    df1 = pd.DataFrame(
+        data1, columns=[STR_CONST_1, "Relative Size of Largest Component", "Mode"]
+    )
+    df2 = pd.DataFrame(
+        data2, columns=[STR_CONST_1, "Diameter of Largest Component", "Mode"]
+    )
+
     # Create a figure and a set of subplots
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
-    # Plotting on the first subplot
-    ax1.grid(True)
-    for mode in attack_modes:
-        x, y, _ = result_dict[mode]
-        ax1.plot(x, y, label=f"{mode}")
-    ax1.set_xlabel("Fraction of Nodes Removed")
-    ax1.set_ylabel("Relative Size of Largest Component")
+    # Plotting using Seaborn on the first subplot
+    sns.lineplot(
+        x=STR_CONST_1,
+        y="Relative Size of Largest Component",
+        hue="Mode",
+        data=df1,
+        ax=ax1,
+    )
     ax1.set_title(f"Robustness Analysis of {graph_name}")
-    ax1.legend()
-
-    # Plotting on the second subplot
-    ax2.grid(True)
-    for mode in attack_modes:
-        x, _, z = result_dict[mode]
-        ax2.plot(x, z, label=f"{mode}")
-    ax2.set_xlabel("Fraction of Nodes Removed")
-    ax2.set_ylabel("Diameter of Largest Component")
+    ax1.grid(True)
+    # Plotting using Seaborn on the second subplot
+    sns.lineplot(
+        x=STR_CONST_1, y="Diameter of Largest Component", hue="Mode", data=df2, ax=ax2
+    )
     ax2.set_title(f"Diameter Changes of {graph_name}")
-    ax2.legend()
+    ax2.grid(True)
 
     # Adjust layout and save the plot
     plt.tight_layout()
@@ -135,53 +151,65 @@ def plot_and_save(graph_name: str, result_dict: dict, attack_modes: list[str]):
     plt.close(fig)  # Close the figure to free up memory
 
 
+def calculate_critical_threshold(graph):
+    degrees = [d for n, d in graph.degree()]
+    k = np.mean(degrees)
+    k2 = np.mean([deg**2 for deg in degrees])
+    if k == 0:
+        return 0
+    return 1 - (k / (k2 - k))
+
+
+def output_results_to_csv(graph_data, filename):
+    df = pd.DataFrame(graph_data)
+    df.to_csv(filename, index=False)
+    print(f"Output saved to {filename}")
+
+
+def calculate_and_save_critical_thresholds(graphs, output_dir):
+    data = [
+        {"Graph": name, "Critical Threshold": calculate_critical_threshold(graph)}
+        for name, graph in graphs.items()
+    ]
+    output_path = Path(__file__).parent.joinpath(output_dir)
+    output_path.mkdir(parents=True, exist_ok=True)
+    with open(
+        output_path.joinpath("critical_thresholds.csv"), mode="w", newline=""
+    ) as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def process_graphs(graphs, attack_modes):
+    result_dict = {}
+    with Pool(processes=len(attack_modes)) as pool:
+        graph_and_modes = [(graph, mode) for graph, mode in graphs]
+        results = pool.map(worker, graph_and_modes)
+        result_dict = {result[0]: result[1:] for result in results}
+    return result_dict
+
+
 if __name__ == "__main__":
     # Setup
     SEED = 2137
-    np.random.seed(SEED)
     random.seed(SEED)
     attack_modes = ["random", "degree", "pagerank", "betweenness", "closeness"]
     mouse_graph, pos, number_of_nodes, average_number_of_edges = setup_graph()
 
-    random_graph = nx.barabasi_albert_graph(n=500, m=int(5), seed=SEED)
-    random_graph = nx.k_core(random_graph, k=2)
-    result_dict = {}
-    results: list = []
-    # temp_list_result = []
-    # for mode in attack_modes:
-    #     print(f"Simulating {mode} attack on random graph...")
-    #     temp_list_result.append(simulate_attacks(deepcopy(random_graph), mode))
-    #     print(f"Simulating {mode} finished...")
-    # result_dict[mode] = simulate_attacks(mouse_graph, mode)
-    # Multiprocessing
+    random_graph = nx.barabasi_albert_graph(
+        n=number_of_nodes, m=int(average_number_of_edges), seed=SEED
+    )
+    # Process critical thresholds and save results
+    graphs = {"mouse-kasthuri-graph-v4": mouse_graph, "random": random_graph}
+    calculate_and_save_critical_thresholds(graphs, "../out")
 
-    with Pool(processes=len(attack_modes)) as pool:
-        graph_and_modes = [(random_graph, mode) for mode in attack_modes]
-        results = pool.map(worker, graph_and_modes)
-    # Organize results in a dictionary
-    result_dict = {result[0]: result[1:] for result in results}
-    # result_dict = {result[0]: result[1:] for result in temp_list_result}
-    print("All simulations completed.")
-    # close the pool
-    # No more tasks can be submitted, tell the pool it can begin shutting down.
-    pool.close()
-    # Wait for all worker processes to finish.
-    pool.join()
-
-    # # Real graph
-    # plot_and_save(
-    #     graph_name="Random Graph", result_dict=result_dict, attack_modes=attack_modes
-    # )
-
-    # with Pool(processes=len(attack_modes)) as pool:
-    #     graph_and_modes = [(mouse_graph, mode) for mode in attack_modes]
-    #     results = pool.map(worker, graph_and_modes)
-    # result_dict = {result[0]: result[1:] for result in results}
-    # print("All simulations completed.")
-    # pool.close()
-    # pool.join()
-    # plot_and_save(
-    #     graph_name="Mouse Brain Graph",
-    #     result_dict=result_dict,
-    #     attack_modes=attack_modes,
-    # )
+    # Process and plot results for random and real graphs
+    for graph_name, graph in graphs.items():
+        print(f"Processing {graph_name} simulations...")
+        graph_and_modes = [(graph, mode) for mode in attack_modes]
+        result_dict = process_graphs(graph_and_modes, attack_modes)
+        plot_and_save(
+            graph_name=graph_name, result_dict=result_dict, attack_modes=attack_modes
+        )
+        print(f"All {graph_name.lower()} simulations completed.")
